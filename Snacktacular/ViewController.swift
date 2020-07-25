3ea62b585da90635a74db7d5c5b6ed520f16bcc8
Romain Francois <romain.francois@gmail.com>
2020-07-25 17:51:09 +0100

    Initial Commit

---
+++ Snacktacular/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snacktacular
+//
+//  Created by Romain Francois on 25/07/2020.
+//  Copyright © 2020 Romain Francois. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
